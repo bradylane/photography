faf91d843fdf84e317b505a4cda4fa69c5b36877
Brady Lane <brady@eccsolar.com>
2021-09-08 16:52:07 -0600

    save scroll

--- gallery.js
+++ gallery.js
@@ -2,9 +2,10 @@ $(document).ready(function () {
 
     let loaded = 0;
     let five = loaded + 5;
+    let scrollTop = sessionStorage.getItem('scrollTop');
 
     function load() {
-        if (loaded < posts.length && $(window).scrollTop() + $(window).height() > $(document).height() - 300) {
+        if (loaded < posts.length && (scrollTop || $(window).scrollTop()) + $(window).height() > $(document).height() - 300) {
             for (var i = loaded; i < five; i++) {
                 $('#gallery').append(`<a href="${posts[i].url}"><img src="/photos/${posts[i].title}-250.jpg" alt="${posts[i].title}"></a>`);
                 loaded++;
@@ -32,6 +33,19 @@ $(document).ready(function () {
         load();
     });
 
-    $(window).scroll(load);
+    $(window).scroll(() => {
+        scrollTop = null;
+        load();
+    });
+
+    let scrollTop = sessionStorage.getItem('scrollTop');
+    if (scrollTop) {
+        $(window).scrollTop(scrollTop);
+        sessionStorage.removeItem('scrollTop');
+    }
+
+    $(window).on('beforeunload', () => {
+        sessionStorage.setItem('scrollTop', $(window).scrollTop());
+    });
 
 });
