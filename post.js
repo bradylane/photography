7015f22125f8fbfca0cc3ed4c1a29c87a08e875e
Brady Lane <brady@eccsolar.com>
2021-09-08 15:38:22 -0600

    fix

--- post.js
+++ post.js
@@ -1,21 +1,3 @@
-document.querySelector('#photo-loading').addEventListener('load', () => {
-    $('#photo-loading').fadeIn();
-});
-
-document.querySelector('#photo img').addEventListener('load', () => {
-    $('#photo-loading').hide();
-    $('#inner-wrap').fadeIn();
-});
-
-document.querySelectorAll('#photo-nav a').forEach(a => {
-    a.addEventListener('click', (event) => {
-        event.preventDefault();
-        $('#inner-wrap').fadeOut(() => {
-            window.location = event.currentTarget.href;
-        });
-    });
-});
-
 document.onkeydown = event => {
     switch (event.key) {
         case 'ArrowLeft':
